12f1d032ccb1c6626eeda5f3eb58f86fb5275cea
HEDI121 <arramihedi50@gmail.com>
2024-06-25 17:21:23 +0100

    lhedi lbanafsagi

--- src/app/pages/examples/register/register.component.ts
+++ src/app/pages/examples/register/register.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { SignupRequest } from "src/app/model/SignupRequest ";
+import { ApiserviceService } from "src/app/services/apiservice.service";
 
 @Component({
   selector: "app-register",
   templateUrl: "register.component.html"
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
   focus;
   focus1;
   focus2;
-  constructor() {}
+  signUpRequest: SignupRequest = {
+    username: '',
+    email: '',
+    password: '',
+    role: ['ROLE_PATIENT'] // Default role set to 'ROLE_PATIENT'
+  };
+  message = '';
+  errorMessage = '';
 
-  ngOnInit() {}
+  constructor(private authService: ApiserviceService, private router: Router) { } // Inject Router
+
+  onSubmit() {
+    this.authService.register(this.signUpRequest).subscribe(
+      data => {
+        this.message = 'User registered successfully! Check your email for verification.';
+        this.errorMessage = '';
+         // Redirect to login page on success
+      },
+      err => {
+        this.errorMessage = err.error.message || 'An error occurred during registration.';
+        this.message = '';
+      }
+    );
+  }
 }
